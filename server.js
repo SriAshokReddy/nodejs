53ed84b9eabeb4bf4351204ecf0a94bc6b6f95e7
SriAshokReddy <50404255+SriAshokReddy@users.noreply.github.com>
2020-12-24 12:47:45 +0530

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var handleRequest = function(request, response) {
-  response.writeHead(200);
-  response.end("<h1> Hello Ashok </h1>");
-}
-var www = http.createServer(handleRequest);
-www.listen(8080);
