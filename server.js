bcd7a984e3883962da1211fa36afb1786dec0cd3
SriAshokReddy <50404255+SriAshokReddy@users.noreply.github.com>
2020-12-25 14:23:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 8080
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Reddy Sab')
+    res.end('Hello Reddy Garu')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
